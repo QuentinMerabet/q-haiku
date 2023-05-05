8cbcee8fb92fc471da4cabf8be7727751b4f8c5c
QuentinMerabet <bonjour@quentinmerabet.com>
2023-05-05 00:47:16 -0400

    checking cache issue

--- src/app/api/getLastTags/route.js
+++ src/app/api/getLastTags/route.js
@@ -2,14 +2,14 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function GET(request) {
-  console.log(request.headers.get("Cache-Control"));
+  console.log(request.headers.get("Cache-Control")); // no-cache
   try {
     const tags = await prisma.tag.findMany({
       orderBy: { id: "desc" },
       take: 3,
     });
     const response = NextResponse.json({ tags });
-    response.headers.set("Cache-Control", "no-store");
+    //response.headers.set("Cache-Control", "no-cache");
     return response;
   } catch (err) {
     console.error(err);
