c586d46108d512552b2540cafac0810127df49cf
QuentinMerabet <bonjour@quentinmerabet.com>
2023-05-05 00:24:26 -0400

    header no-store

--- src/app/api/getLastTags/route.js
+++ src/app/api/getLastTags/route.js
@@ -1,14 +1,20 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET() {
+export async function GET(request) {
   try {
     const tags = await prisma.tag.findMany({
       orderBy: { id: "desc" },
       take: 3,
     });
-
-    return NextResponse.json({ tags });
+    return NextResponse.json(
+      { tags },
+      {
+        headers: {
+          "Cache-Control": "no-store",
+        },
+      }
+    );
   } catch (err) {
     console.error(err);
   }
