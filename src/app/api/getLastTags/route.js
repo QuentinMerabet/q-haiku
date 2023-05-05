bbb12c21ea5c9e7ca5f814266cfc30efbe6b56ee
QuentinMerabet <bonjour@quentinmerabet.com>
2023-05-05 00:43:58 -0400

    another try

--- src/app/api/getLastTags/route.js
+++ src/app/api/getLastTags/route.js
@@ -2,19 +2,15 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function GET(request) {
+  console.log(request.headers.get("Cache-Control"));
   try {
     const tags = await prisma.tag.findMany({
       orderBy: { id: "desc" },
       take: 3,
     });
-    return NextResponse.json(
-      { tags },
-      {
-        headers: {
-          "Cache-Control": "no-store",
-        },
-      }
-    );
+    const response = NextResponse.json({ tags });
+    response.headers.set("Cache-Control", "no-store");
+    return response;
   } catch (err) {
     console.error(err);
   }
